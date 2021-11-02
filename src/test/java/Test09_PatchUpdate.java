aab2cf0c71efea52cc50f197ef0a6c6c26339bfa
Pranjal Rawat <pranjal.rawat@QUALITEST.LOCAL>
2021-11-02 11:37:10 +0530

    Test Patch Update Test Added

---
+++ src/test/java/Test09_PatchUpdate.java
@@ -0,0 +1,20 @@
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test09_PatchUpdate extends ResponseFile{
+    @Test
+    public void patchUpdate(){
+        JSONObject request = new JSONObject();
+        request.put("name","oliver");
+        request.put("job","DDA");
+
+        System.out.println(request);
+
+        given()
+                .body(request.toJSONString())
+                .when().patch("https://reqres.in/api/users/2")
+                .then().statusCode(200);
+    }
+}
